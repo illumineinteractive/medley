63bb1ca2a80a8d299f2ce09cd6a960ef2b60c7d5
Garret Bolthouse <garret@theenvoygroup.com>
2019-05-06 14:26:45 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="medley",
-    version="1.0.3",
+    version="1.0.4",
     author="Garret Bolthouse",
     author_email="garret@illumineinteractive.com",
     description="A simple, lightweight Python Dependency Injection Container (IOC), inspired by Pimple",
